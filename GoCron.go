d02be6823dfeb104de6d7cc330e4c7557d7cb268
Michael.Obermueller <michael.obermueller@dynatrace.com>
2018-10-30 16:11:51 +0100

    simplified GoCron

--- GoCron.go
+++ GoCron.go
@@ -27,20 +27,20 @@ func main() {
 	http.ListenAndServe("127.0.0.1:8000", nil)
 }
 
-// CronJob 
 type CronJob struct {
-	Cmd       string
-	OnSuccess func(cmd, output string)
-	OnError   func(cmd string, err error)
+	Cmd string
 }
 
 // Run is called by the scheduler to execute the cron job
 func (j *CronJob) Run() {
+	report := fmt.Sprintf("Command %s output:\n", j.Cmd)
 	if output, err := exec.Command(j.Cmd).CombinedOutput(); err == nil {
-		j.OnSuccess(j.Cmd, string(output))
+		report += string(output)
 	} else {
-		j.OnError(j.Cmd, err)
+		report += err.Error()
 	}
+
+	http.Post(reportToUrl, "text/plain", strings.NewReader(report))
 }
 
 // registerJobHandler is called for incoming job registration requests
@@ -48,18 +48,7 @@ func registerJobHandler(w http.ResponseWriter, r *http.Request) {
 	cmd := filepath.Join(workingDir, r.URL.Query().Get("command"))
 	schedule := r.URL.Query().Get("schedule")
 
-	err := jobRunner.AddJob(schedule, &CronJob{Cmd: cmd,
-		OnSuccess: func(cmd, output string) {
-			body := fmt.Sprintf("Command %s succeeded, output:\n%s", cmd, output)
-			http.Post(reportToUrl, "text/plain", strings.NewReader(body))
-		},
-		OnError: func(cmd string, err error) {
-			body := fmt.Sprintf("Command %s failed: %s", cmd, err.Error())
-			http.Post(reportToUrl, "text/plain", strings.NewReader(body))
-		},
-	})
-
-	if err != nil {
+	if err := jobRunner.AddJob(schedule, &CronJob{Cmd: cmd}); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 	}
 }
